896315d0a6df6964a89a0601e9e2af724accccb0
WitherKNyx <witherkedward@gmail.com>
2022-11-16 19:35:55 -0500

    Added actual code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("To be made later.");
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err("ERROR: Invalid arguments.\nExpected: ./ternary [input_filename]");
+    }
+    let input_filename = args[1].clone();
+    let ternary_string: String = fs::read_to_string(input_filename).expect("ERROR: Could not read file.");
+    ternary_string.split(" ");
+    Ok(())
 }
